318254a88b631f87888ad362cb5d20b1e4ecaec5
Jose E Reyes <jreyes8631@gmail.com>
2020-03-14 22:09:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,10 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  #returns an array of words that are five letters long
+  return_array = text.split(" ")
+  return_array.grep(/\b\w{5}\b/)
+  #return_array.grep(/\b+\w{4}+\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
